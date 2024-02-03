8be6a214a1ad0c27820e27cf77102166aaf44abf
essential-accident <essentialaccident@gmail.com>
2024-02-03 09:21:46 -0500

    Got the tank to rotate with movement keys

--- lib/tank.rb
+++ lib/tank.rb
@@ -10,20 +10,35 @@ class Tank < GameObject
   def initialize(window, position = Vector2d.new(0, 0), color = 'red')
     super window, position
     @image = get_image('tank', color)
+    @angle_rot = 0
   end
 
-  def update; end
+  def update
+    # Updates to the tank go here
+  end
 
   def draw
-    @image.draw(@position.x, @position.y)
+    # Image.draw_rot centers the image on the position
+    # For that reason, the drawing position needs to be fixed
+    @image.draw_rot(
+      @position.x + (@image.width / 2),
+      @position.y + (@image.height / 2),
+      10,
+      @angle_rot
+    )
   end
 
   def move(direction)
     case direction
     when :up
+      @angle_rot = 0
     when :down
-    when :left
+      @angle_rot = 180
     when :right
+      @angle_rot = 90
+    when :left
+      @angle_rot = 270
+
     end
   end
 end
--- tankz.rb
+++ tankz.rb
@@ -15,22 +15,34 @@ class Tankz < Gosu::Window
     super WINDOW_WIDTH, WINDOW_HEIGHT
     self.caption = 'Tankz'
 
-    @tank = Tank.new(self, Vector2d.new(0, 0), 'black')
+    @player = Tank.new(self, Vector2d.new(0, 0), 'green')
   end
 
   def update
     # Update the game one frame at the time
+    @player.update
   end
 
   def draw
     # Draw the game one frame at the time
-    @tank.draw
+    @player.draw
   end
 
-  def button_down(id)
-    case id
+  def button_down(button_id)
+    case button_id
+    # When Esc key is pressed, game quits
     when Gosu::KB_ESCAPE
       close
+    # Tank movement
+    # The tank moves with keys W, A, S, D
+    when Gosu::KB_W
+      @player.move :up
+    when Gosu::KB_S
+      @player.move :down
+    when Gosu::KB_D
+      @player.move :right
+    when Gosu::KB_A
+      @player.move :left
     end
   end
 end
